c0aeda0a772c77419c5bd0437504c02e47efba0a
Andrés Ospina <adrosscosmic@gmail.com>
2026-05-17 00:30:52 -0500

    .

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -10,20 +10,69 @@ use App\Models\Post;
 class PostController extends BaseController
 {
     private Post $post;
+    private int $post_id;
 
-    public function __construct()
+    public function __construct(array $args)
     {
         $this->post = new Post;
+
+        if (!empty($args)) {
+            $this->post_id = $args[0];
+        }
+    }
+
+    private function parseFieldsToUpdate(array $post, array $new_post): array
+    {
+        $to_update = $post;
+
+        if (isset($new_post['title']) && !empty($new_post['title'])) {
+            if (strlen($new_post['title']) <= 50) {
+                $to_update['title'] = $this->sanitizeText($new_post['title']);
+            }
+        }
+
+        if (isset($new_post['content'])) {
+            if (strlen($new_post['content']) <= 1500) {
+                $to_update['content'] = trim($new_post['content']);
+            }
+        }
+
+        if (isset($new_post['excerpt'])) {
+            $to_update['excerpt'] = $this->sanitizeText($new_post['excerpt']);
+        }
+
+        $to_update['slug'] = $this->getSlug($new_post['title']);
+        $to_update['terms'] = [];
+
+        if (isset($new_post['terms']) && count($new_post['terms']) > 0) {
+            $to_update['terms'] = array_map('intval', $new_post['terms']);
+            $to_update['terms'] = array_filter($new_post['terms'], fn($id) => $id > 0);
+        }
+
+        return $to_update;
     }
 
     public function getAll(): Response
     {
-        return $this->error("Pendiente por implementar");
+        // TODO implementar query params
+        try {
+            $result = $this->post->getAll();
+            return $this->success($result);
+        } catch (\Throwable $e) {
+            error_log($e->getMessage());
+            return $this->error("No se ha podido obtener la información.");
+        }
     }
 
     public function get(): Response
     {
-        return $this->error("Pendiente por implementar");
+        try {
+            $result = $this->post->get($this->post_id);
+            return $this->success($result);
+        } catch (\Throwable $e) {
+            error_log($e->getMessage());
+            return $this->error($e->getMessage(), 404);
+        }
     }
 
     public function create(array $data): Response
@@ -71,9 +120,28 @@ class PostController extends BaseController
         }
     }
 
-    public function update(): Response
+    public function update(array $data): Response
     {
-        return $this->error("Pendiente por implementar");
+        if (!isset($this->post_id)) {
+            return $this->error("No se ha especificado el post a editar.", 412);
+        }
+
+        try {
+            $post = $this->post->get($this->post_id);
+        } catch (\Throwable $e) {
+            error_log($e->getMessage());
+            return $this->error($e->getMessage(), 404);
+        }
+
+        $new_post = $this->parseFieldsToUpdate($post, $data);
+
+        try {
+            $this->post->update($new_post);
+            return $this->success(["message" => "'{$new_post['title']}' se ha creado correctamente."]);
+        } catch (\Throwable $e) {
+            error_log($e->getMessage());
+            return $this->error($e->getMessage(), 409);
+        }
     }
 
     public function delete(): Response
--- src/Models/Post.php
+++ src/Models/Post.php
@@ -49,14 +49,103 @@ class Post extends BaseModel
         $stmt->execute($params);
     }
 
-    public function getAll(): array
+    private function getPostTerms(int $post_id): array
     {
-        return [];
+        $query = "
+            SELECT pt.term_id, t.name as term_name, t.slug as term_slug, t.taxonomy_id, tt.name as taxonomy_name, tt.slug as taxonomy_slug, tm.id as term_meta_id, tm.meta_key, tm.meta_value
+            FROM `post_terms` as pt
+            INNER JOIN terms as t
+            ON t.id = pt.term_id
+            INNER JOIN taxonomies as tt
+            ON tt.id = t.taxonomy_id
+            LEFT JOIN term_meta as tm
+            ON t.id = tm.term_id
+            WHERE post_id = :post_id";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->execute([':post_id' => $post_id]);
+
+        $result = $stmt->fetchAll();
+
+        $taxonomies = [];
+
+        foreach ($result as $row) {
+            $taxonomy_slug = $row['taxonomy_slug'];
+            $term_id = $row['term_id'];
+
+            if (!isset($taxonomies[$taxonomy_slug])) {
+                $taxonomies[$taxonomy_slug] = [
+                    'taxonomy_id'   => $row['taxonomy_id'],
+                    'taxonomy_name' => $row['taxonomy_name'],
+                    'taxonomy_slug' => $taxonomy_slug,
+                    'terms'         => []
+                ];
+            }
+
+            if (!isset($taxonomies[$taxonomy_slug]['terms'][$term_id])) {
+                $taxonomies[$taxonomy_slug]['terms'][$term_id] = [
+                    'term_id'   => $term_id,
+                    'term_name' => $row['term_name'],
+                    'term_slug' => $row['term_slug'],
+                    'meta'      => []
+                ];
+            }
+
+            if ($row['meta_key'] !== null) {
+                $taxonomies[$taxonomy_slug]['terms'][$term_id]['meta'][] = [
+                    'meta_key'   => $row['meta_key'],
+                    'meta_value' => $row['meta_value']
+                ];
+            }
+        }
+
+        foreach ($taxonomies as &$taxonomy) {
+            $taxonomy['terms'] = array_values($taxonomy['terms']);
+        }
+
+        return [$taxonomies];
     }
 
-    public function get(int $int = 01): array
+    public function getAll(): array
     {
-        return [];
+        $query = "SELECT id, title, slug, content, excerpt, published_at FROM posts";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->execute();
+
+        $posts = $stmt->fetchAll();
+
+        foreach ($posts as $key => $post) {
+            $id = $post['id'];
+            $terms = $this->getPostTerms($id);
+            $posts[$key]['terms'] = [$terms];
+        }
+
+        return $posts;
+    }
+
+    public function get(int $post_id): array
+    {
+        $query = "SELECT id, title, slug, content, excerpt, published_at FROM posts WHERE id = :post_id";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->execute([':post_id' => $post_id]);
+
+        $post = $stmt->fetch();
+
+        if (!$post) {
+            throw new \InvalidArgumentException("No se encontró el post con el id indicado");
+        }
+
+        $id = $post['id'];
+        $terms = $this->getPostTerms($id);
+
+        $post['terms'] = [$terms];
+
+        return $post;
     }
 
     public function create(array $data): array
@@ -103,8 +192,49 @@ class Post extends BaseModel
         return [];
     }
 
-    public function update(int $int = 01): array
+    public function update(array $data): array
     {
+        [
+            'id'      => $post_id,
+            'title'   => $title,
+            'slug'    => $slug,
+            'content' => $content,
+            'excerpt' => $excerpt,
+            'terms'   => $terms
+        ] = $data;
+
+        $status = 'published';
+
+        if ($this->itemExist('posts', $slug)) {
+            throw new \InvalidArgumentException("El slug está duplicado");
+        }
+
+        $query = "  UPDATE posts 
+                    SET title = :title, slug = :slug, content = :content, excerpt = :excerpt, status = :published, published_at = NOW()
+                    WHERE id = :post_id";
+
+        $stmt = $this->db->prepare($query);
+
+        $this->db->beginTransaction();
+
+        try {
+            $stmt->execute([
+                ":title"     => $title,
+                ":slug"      => $slug,
+                ":content"   => $content,
+                ":excerpt"   => $excerpt,
+                ":published" => $status,
+                ":post_id"   => $post_id,
+            ]);
+
+            $this->insertTerms($post_id, $terms);
+
+            $this->db->commit();
+        } catch (\Throwable $e) {
+            $this->db->rollBack();
+            throw $e;
+        }
+
         return [];
     }
 
